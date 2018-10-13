d7a980a3243104748a26f070883addb0eba76c21
Niko Wenselowski <der@nik0.de>
2018-10-13 21:42:46 +0200

    Preparing for packaging.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="tornado-jsonrpc2",
+    version="0.1",
+    author="Niko Wenselowski",
+    author_email="niko@nerdno.de",
+    description="A handler for JSON-RPC for Tornado.",
+    url="https://github.com/okin/tornado-jsonrpc2",
+    packages=setuptools.find_packages(),
+    install_requires=['tornado>=5.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
