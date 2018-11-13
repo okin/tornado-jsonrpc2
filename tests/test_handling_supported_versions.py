9966b1583264bbfc3d5eae5cd33eec09262fb74f
Niko Wenselowski <der@nik0.de>
2018-11-14 00:10:03 +0100

    Add an test for handling different versions when no version restriction is given.

---
+++ tests/test_handling_supported_versions.py
@@ -0,0 +1,59 @@
+import pytest
+import functools
+import tornado.web
+from tornado.escape import json_encode, json_decode
+
+from tornado_jsonrpc2.handler import JSONRPCHandler
+
+
+@pytest.fixture
+def app(request):
+    async def version(request):
+        return request.version
+
+    return tornado.web.Application([
+        (r"/jsonrpc", JSONRPCHandler, {"response_creator": version}),
+    ])
+
+
+@pytest.fixture
+def test_url(base_url):
+    return base_url + '/jsonrpc'
+
+
+@pytest.fixture
+def jsonrpc_fetch(http_client, test_url):
+    return functools.partial(
+        http_client.fetch,
+        test_url,
+        method="POST",
+        headers={'Content-Type': 'application/json'},
+    )
+
+
+@pytest.mark.gen_test
+def test_mixing_handled_versions(jsonrpc_fetch):
+    v1Request = {
+        "method": "foo",
+        "params": [],
+        "id": 1
+    }
+
+    response = yield jsonrpc_fetch(body=json_encode(v1Request))
+    assert 200 == response.code
+    response = json_decode(response.body)
+    assert response['id'] == v1Request['id']
+    assert not response['error']
+    assert response['result'] == '1.0'
+
+    v2Request = {
+        "jsonrpc": "2.0",
+        "method": "foo",
+        "id": 2
+    }
+    response = yield jsonrpc_fetch(body=json_encode(v2Request))
+    assert 200 == response.code
+    response = json_decode(response.body)
+    assert response['id'] == v2Request['id']
+    assert 'error' not in response
+    assert response['result'] == '2.0'
