32d22bcb39a59e3672187922b53db94568d717c0
Niko Wenselowski <der@nik0.de>
2018-11-13 23:21:19 +0100

    Working on new version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tornado-jsonrpc2",
-    version="0.1",
+    version="0.2.dev",
     author="Niko Wenselowski",
     author_email="niko@nerdno.de",
     description="JSON-RPC 2.0 handler for Tornado.",
