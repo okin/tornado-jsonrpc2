6f7bf22ab862ab7dd4bcf24869c60b148cfda0ae
Niko Wenselowski <der@nik0.de>
2018-11-14 00:10:32 +0100

    Use tornado JSON functions.

--- tests/test_jsonrpc_2_errorhandling.py
+++ tests/test_jsonrpc_2_errorhandling.py
@@ -2,9 +2,9 @@
 Errorhandling tests for JSONRPC 2.0.
 """
 
-import json
 import pytest
 import tornado
+from tornado.escape import json_encode, json_decode
 
 from .test_jsonrpc_2_spec import jsonrpc_fetch, test_url
 from tornado_jsonrpc2 import JSONRPCHandler
@@ -23,11 +23,11 @@ def app():
 @pytest.mark.gen_test
 def test_invalid_error_in_responder(jsonrpc_fetch):
     response = yield jsonrpc_fetch(
-        body=json.dumps({"jsonrpc": "2.0", "method": "foo", "id": 1})
+        body=json_encode({"jsonrpc": "2.0", "method": "foo", "id": 1})
     )
     assert 200 == response.code
 
-    response = json.loads(response.body)
+    response = json_decode(response.body)
     expected_response = {
         'jsonrpc': '2.0',
         'id': 1,
@@ -42,11 +42,11 @@ def test_invalid_error_in_responder(jsonrpc_fetch):
 @pytest.mark.gen_test
 def test_invalid_request(jsonrpc_fetch):
     response = yield jsonrpc_fetch(
-        body=json.dumps({"jsonrpc": "2.0", "id": 1})
+        body=json_encode({"jsonrpc": "2.0", "id": 1})
     )
     assert 200 == response.code
 
-    response = json.loads(response.body)
+    response = json_decode(response.body)
     expected_response = {
         'error': {
             'code': -32600,
@@ -61,11 +61,11 @@ def test_invalid_request(jsonrpc_fetch):
 @pytest.mark.gen_test
 def test_unsupported_jsonrpc_version(jsonrpc_fetch):
     response = yield jsonrpc_fetch(
-        body=json.dumps({"jsonrpc": "3000", "id": 1, "method": "foo"})
+        body=json_encode({"jsonrpc": "3000", "id": 1, "method": "foo"})
     )
     assert 200 == response.code
 
-    response = json.loads(response.body)
+    response = json_decode(response.body)
     expected_response = {
         'error': {
             'code': -32600,
