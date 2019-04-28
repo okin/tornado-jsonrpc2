4b144f1ec9a39a95f6c350cd552998b1fc908ccc
Niko Wenselowski <der@nik0.de>
2019-04-28 10:06:58 +0200

    pytest-tornado is back in maintained mode.

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=['tornado>=5.0'],
     extras_require={
-        'test': ['pytest-tornado5'],
+        'test': ['pytest-tornado>=0.6'],
     },
-    tests_require=['pytest-tornado5'],
+    tests_require=['pytest-tornado>=0.6'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
